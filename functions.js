bf5faa482e07a3f3a021286a8efa92f69d5213db
Gabriel Muri <65788953+gabrielmuri@users.noreply.github.com>
2021-02-02 21:56:40 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,17 @@
-jQuery(document).ready(function()
+$(document).ready(function()
 {
     slideToSection();
 });
 
 function slideToSection()
 {
-
     $('.nav-link').click(function(e){
         e.preventDefault();
         const _doc = $('html, body');
         const href = $(this).attr('href');
 
-        _doc.animate({scrollTop: $( href ).offset().top
+        _doc.animate({
+            scrollTop: $( href ).offset().top
         }, 500);
-    })
-    
+    });
 };
